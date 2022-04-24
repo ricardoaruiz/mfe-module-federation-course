e92cad364af6250ab6f917bb51173f477957a686
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2022-04-24 11:17:55 -0300

    fix: adding publicPath on container webpack production configuration

--- packages/container/webpack/webpack.prod.js
+++ packages/container/webpack/webpack.prod.js
@@ -3,12 +3,13 @@ const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const commonConfig = require('./webpack.common')
 
-module.exports = (env) => {
+module.exports = () => {
   const prodConfig = {
     mode: 'production',
     output: {
       filename: '[name].[contenthash].js',
       path: path.resolve(__dirname, '..', 'dist'),
+      publicPath: '/container/latest/',
     },
     plugins: [
       new HtmlWebpackPlugin({
