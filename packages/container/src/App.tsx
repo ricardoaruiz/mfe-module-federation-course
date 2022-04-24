c0239003ae56ac8397b65e84805c34aa7b37d540
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2022-04-24 14:08:49 -0300

    chore: adding githubactions for container project

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const App: React.FC = () => {
-  return <h1>Container works fine!! Now with React and Typescript</h1>
+  return (
+    <>
+      <h1>Container works fine!! Now with React and Typescript</h1>
+      <h2>Deployed by GithubActions on AWS S3 and Cloudfront</h2>
+    </>
+  )
 }
 
 export default App
