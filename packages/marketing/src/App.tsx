3f9c127b7640dff4de0149ffb803c910eeb5d6cd
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2022-04-25 17:55:47 -0300

    feat: create githubactions for marketing project

--- packages/marketing/src/App.tsx
+++ packages/marketing/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App: React.FC = () => {
-  return <h1>Marketing works fine!1</h1>
+  return <h1>Marketing works fine!!!! Deployed by GithubActions</h1>
 }
 
 export default App
